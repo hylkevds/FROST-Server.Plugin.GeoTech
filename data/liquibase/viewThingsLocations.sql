2662bcc12c18cd5a6baff6aa9b6ee254d43524ae
Hylke van der Schaaf <hylke.vds@gmail.com>
2023-06-01 14:20:07 +0200

    Fixed column names in link view THINGS_LOCATIONS

--- data/liquibase/viewThingsLocations.sql
+++ data/liquibase/viewThingsLocations.sql
@@ -1,10 +1,11 @@
+DROP VIEW "THINGS_LOCATIONS";
 CREATE OR REPLACE VIEW "THINGS_LOCATIONS"
 AS SELECT
-    "BH_CollarThings_LOCATIONS"."BH_CollarThing_ID",
-    "BH_CollarThings_LOCATIONS"."LOCATION_ID"
+    "BH_CollarThings_LOCATIONS"."BH_CollarThing_ID" as "THING_ID",
+    "BH_CollarThings_LOCATIONS"."LOCATION_ID" as "LOCATION_ID"
 FROM "BH_CollarThings_LOCATIONS"
 UNION
 SELECT
-    "BH_TrajectoryThings_LOCATIONS"."BH_TrajectoryThing_ID",
-    "BH_TrajectoryThings_LOCATIONS"."LOCATION_ID"
+    "BH_TrajectoryThings_LOCATIONS"."BH_TrajectoryThing_ID" as "THING_ID",
+    "BH_TrajectoryThings_LOCATIONS"."LOCATION_ID" as "LOCATION_ID"
 FROM "BH_TrajectoryThings_LOCATIONS";
