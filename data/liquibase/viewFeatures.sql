385e1e942a739db52a3947a7661d359f37f486b0
Hylke van der Schaaf <hylke.vds@gmail.com>
2023-06-06 23:13:24 +0200

    Fixed GEOM column name in FEATURES view

--- data/liquibase/viewFeatures.sql
+++ data/liquibase/viewFeatures.sql
@@ -6,7 +6,7 @@ AS SELECT
     "BH_HoleFoIs"."DESCRIPTION",
     "BH_HoleFoIs"."ENCODING_TYPE",
     ST_AsGeoJSON("BH_HoleFoIs"."FEATURE") as "FEATURE",
-    "BH_HoleFoIs"."FEATURE" as GEOM,
+    "BH_HoleFoIs"."FEATURE" as "GEOM",
     ((json_build_object('length', "BH_HoleFoIs"."LENGTH", 'length_uom', "BH_HoleFoIs"."LENGTH_UOM"))::jsonb || ("BH_HoleFoIs"."PROPERTIES")::jsonb) AS "PROPERTIES"
 FROM "BH_HoleFoIs"
 UNION
@@ -16,7 +16,7 @@ SELECT
     "BH_CoreFoIs"."DESCRIPTION",
     "BH_CoreFoIs"."ENCODING_TYPE",
     ST_AsGeoJSON("BH_CoreFoIs"."FEATURE") as "FEATURE",
-    "BH_CoreFoIs"."FEATURE" as GEOM,
+    "BH_CoreFoIs"."FEATURE" as "GEOM",
     ((json_build_object('length', "BH_CoreFoIs"."LENGTH", 'length_uom', "BH_CoreFoIs"."LENGTH_UOM"))::jsonb || ("BH_CoreFoIs"."PROPERTIES")::jsonb) AS "PROPERTIES"
 FROM "BH_CoreFoIs"
 UNION
@@ -26,7 +26,7 @@ SELECT
     "BH_HolePointFoIs"."DESCRIPTION",
     "BH_HolePointFoIs"."ENCODING_TYPE",
     ST_AsGeoJSON("BH_HolePointFoIs"."FEATURE") as "FEATURE",
-    "BH_HolePointFoIs"."FEATURE" as GEOM,
+    "BH_HolePointFoIs"."FEATURE" as "GEOM",
     ("BH_HolePointFoIs"."PROPERTIES")::jsonb AS "PROPERTIES"
 FROM "BH_HolePointFoIs"
 UNION
@@ -36,7 +36,7 @@ SELECT
     "BH_CorePointFoIs"."DESCRIPTION",
     "BH_CorePointFoIs"."ENCODING_TYPE",
     ST_AsGeoJSON("BH_CorePointFoIs"."FEATURE") as "FEATURE",
-    "BH_CorePointFoIs"."FEATURE" as GEOM,
+    "BH_CorePointFoIs"."FEATURE" as "GEOM",
     ("BH_CorePointFoIs"."PROPERTIES")::jsonb AS "PROPERTIES"
 FROM "BH_CorePointFoIs"
 UNION
@@ -46,7 +46,7 @@ SELECT
     "BH_HoleSegmentFoIs"."DESCRIPTION",
     "BH_HoleSegmentFoIs"."ENCODING_TYPE",
     ST_AsGeoJSON("BH_HoleSegmentFoIs"."FEATURE") as "FEATURE",
-    "BH_HoleSegmentFoIs"."FEATURE" as GEOM,
+    "BH_HoleSegmentFoIs"."FEATURE" as "GEOM",
     ((json_build_object('length', "BH_HoleSegmentFoIs"."LENGTH", 'length_uom', "BH_HoleSegmentFoIs"."LENGTH_UOM"))::jsonb || ("BH_HoleSegmentFoIs"."PROPERTIES")::jsonb) AS "PROPERTIES"
 FROM "BH_HoleSegmentFoIs"
 UNION
@@ -56,6 +56,6 @@ SELECT
     "BH_CoreSegmentFoIs"."DESCRIPTION",
     "BH_CoreSegmentFoIs"."ENCODING_TYPE",
     ST_AsGeoJSON("BH_CoreSegmentFoIs"."FEATURE") as "FEATURE",
-    "BH_CoreSegmentFoIs"."FEATURE" as GEOM,
+    "BH_CoreSegmentFoIs"."FEATURE" as "GEOM",
     ((json_build_object('length', "BH_CoreSegmentFoIs"."LENGTH", 'length_uom', "BH_CoreSegmentFoIs"."LENGTH_UOM", 'recovery_percentage', "BH_CoreSegmentFoIs"."RECOVERY_PERCENTAGE"))::jsonb || ("BH_CoreSegmentFoIs"."PROPERTIES")::jsonb) AS "PROPERTIES"
 FROM "BH_CoreSegmentFoIs";
