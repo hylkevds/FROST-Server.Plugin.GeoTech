c0b73d573cee6018485885cf3b3640f28e4467f6
Hylke van der Schaaf <hylke.vds@gmail.com>
2023-06-23 17:21:42 +0200

    Redesigned Features and FeatureTypes model

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM fraunhoferiosb/frost-server-http:develop-2.x-2.2.0-SNAPSHOT
 
-COPY data ${CATALINA_HOME}/webapps/FROST-Server/WEB-INF/data
+COPY data ${CATALINA_HOME}/webapps/FROST-Server/WEB-INF/classes/data
 COPY index.html ${CATALINA_HOME}/webapps/FROST-Server/
 
 ENV plugins_coreModel_enable=true
@@ -9,9 +9,9 @@ ENV plugins_multiDatastream_enable=false
 ENV plugins_openApi_enable=true
 ENV plugins_odata_enable=true
 ENV plugins_modelLoader_enable=true
-ENV plugins_modelLoader_modelPath='webapps/FROST-Server/WEB-INF/data/model'
-ENV plugins_modelLoader_modelFiles='Sensor.json, BhCollarThing.json, BhCoreFoi.json, BhCoreFoiType.json, BhCorePointFoi.json, BhCorePointFoiType.json, BhCoreSegmentFoi.json, BhCoreSegmentFoiType.json, BhHoleFoi.json, BhHoleFoiType.json, BhHolePointFoi.json, BhHolePointFoiType.json, BhHoleSegmentFoi.json, BhHoleSegmentFoiType.json, BhPreparationProcedure.json, BhPreparationStep.json, BhSampler.json, BhSampling.json, BhSamplingProcedure.json, BhTrajectoryThing.json, Datastream.json, Location.json, Observation.json'
-ENV plugins_modelLoader_liquibasePath='../data/liquibase'
+ENV plugins_modelLoader_modelPath='webapps/FROST-Server/WEB-INF/classes/data/model'
+ENV plugins_modelLoader_modelFiles='BhCollarThing.json, BhFeature.json, BhFeatureType.json, BhPreparationProcedure.json, BhPreparationStep.json, BhSampler.json, BhSampling.json, BhSamplingProcedure.json, BhTrajectoryThing.json, Datastream.json, Location.json, Observation.json, Sensor.json'
+ENV plugins_modelLoader_liquibasePath='data/liquibase'
 ENV plugins_modelLoader_liquibaseFiles='geoTech.xml'
 
 USER tomcat
--- data/liquibase/viewFeatures.sql
+++ data/liquibase/viewFeatures.sql
@@ -1,61 +1,11 @@
 DROP VIEW IF EXISTS "FEATURES";
 CREATE OR REPLACE VIEW "FEATURES"
 AS SELECT
-    "BH_HoleFoIs"."ID",
-    "BH_HoleFoIs"."NAME",
-    "BH_HoleFoIs"."DESCRIPTION",
-    "BH_HoleFoIs"."ENCODING_TYPE",
-    ST_AsGeoJSON("BH_HoleFoIs"."FEATURE") as "FEATURE",
-    "BH_HoleFoIs"."FEATURE" as "GEOM",
-    ((json_build_object('length', "BH_HoleFoIs"."LENGTH", 'length_uom', "BH_HoleFoIs"."LENGTH_UOM"))::jsonb || ("BH_HoleFoIs"."PROPERTIES")::jsonb) AS "PROPERTIES"
-FROM "BH_HoleFoIs"
-UNION
-SELECT
-    "BH_CoreFoIs"."ID",
-    "BH_CoreFoIs"."NAME",
-    "BH_CoreFoIs"."DESCRIPTION",
-    "BH_CoreFoIs"."ENCODING_TYPE",
-    ST_AsGeoJSON("BH_CoreFoIs"."FEATURE") as "FEATURE",
-    "BH_CoreFoIs"."FEATURE" as "GEOM",
-    ((json_build_object('length', "BH_CoreFoIs"."LENGTH", 'length_uom', "BH_CoreFoIs"."LENGTH_UOM"))::jsonb || ("BH_CoreFoIs"."PROPERTIES")::jsonb) AS "PROPERTIES"
-FROM "BH_CoreFoIs"
-UNION
-SELECT
-    "BH_HolePointFoIs"."ID",
-    "BH_HolePointFoIs"."NAME",
-    "BH_HolePointFoIs"."DESCRIPTION",
-    "BH_HolePointFoIs"."ENCODING_TYPE",
-    ST_AsGeoJSON("BH_HolePointFoIs"."FEATURE") as "FEATURE",
-    "BH_HolePointFoIs"."FEATURE" as "GEOM",
-    ("BH_HolePointFoIs"."PROPERTIES")::jsonb AS "PROPERTIES"
-FROM "BH_HolePointFoIs"
-UNION
-SELECT
-    "BH_CorePointFoIs"."ID",
-    "BH_CorePointFoIs"."NAME",
-    "BH_CorePointFoIs"."DESCRIPTION",
-    "BH_CorePointFoIs"."ENCODING_TYPE",
-    ST_AsGeoJSON("BH_CorePointFoIs"."FEATURE") as "FEATURE",
-    "BH_CorePointFoIs"."FEATURE" as "GEOM",
-    ("BH_CorePointFoIs"."PROPERTIES")::jsonb AS "PROPERTIES"
-FROM "BH_CorePointFoIs"
-UNION
-SELECT
-    "BH_HoleSegmentFoIs"."ID",
-    "BH_HoleSegmentFoIs"."NAME",
-    "BH_HoleSegmentFoIs"."DESCRIPTION",
-    "BH_HoleSegmentFoIs"."ENCODING_TYPE",
-    ST_AsGeoJSON("BH_HoleSegmentFoIs"."FEATURE") as "FEATURE",
-    "BH_HoleSegmentFoIs"."FEATURE" as "GEOM",
-    ((json_build_object('length', "BH_HoleSegmentFoIs"."LENGTH", 'length_uom', "BH_HoleSegmentFoIs"."LENGTH_UOM"))::jsonb || ("BH_HoleSegmentFoIs"."PROPERTIES")::jsonb) AS "PROPERTIES"
-FROM "BH_HoleSegmentFoIs"
-UNION
-SELECT
-    "BH_CoreSegmentFoIs"."ID",
-    "BH_CoreSegmentFoIs"."NAME",
-    "BH_CoreSegmentFoIs"."DESCRIPTION",
-    "BH_CoreSegmentFoIs"."ENCODING_TYPE",
-    ST_AsGeoJSON("BH_CoreSegmentFoIs"."FEATURE") as "FEATURE",
-    "BH_CoreSegmentFoIs"."FEATURE" as "GEOM",
-    ((json_build_object('length', "BH_CoreSegmentFoIs"."LENGTH", 'length_uom', "BH_CoreSegmentFoIs"."LENGTH_UOM", 'recovery_percentage', "BH_CoreSegmentFoIs"."RECOVERY_PERCENTAGE"))::jsonb || ("BH_CoreSegmentFoIs"."PROPERTIES")::jsonb) AS "PROPERTIES"
-FROM "BH_CoreSegmentFoIs";
+    "BH_Features"."ID",
+    "BH_Features"."NAME",
+    "BH_Features"."DESCRIPTION",
+    "BH_Features"."ENCODING_TYPE",
+    ST_AsGeoJSON("BH_Features"."FEATURE") as "FEATURE",
+    "BH_Features"."FEATURE" as "GEOM",
+    ((json_build_object('length', "BH_Features"."LENGTH", 'length_uom', "BH_Features"."LENGTH_UOM", 'recovery_percentage', "BH_Features"."RECOVERY_PERCENTAGE"))::jsonb || ("BH_Features"."PROPERTIES")::jsonb) AS "PROPERTIES"
+FROM "BH_Features";
